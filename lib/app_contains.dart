16f8b7e53d34ed00fd36f2edcc91e670557c5088
mayan <mayanksanecha433@gmail.com>
2024-10-03 18:35:04 +0530

    Initial commit

---
+++ lib/app_contains.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class Portrait {
+  static final List<Map<String,dynamic>>wall =[
+
+    {'WallPaper':'lib/asstes/images/andrzej.jpg'},
+    {'WallPaper':'lib/asstes/images/arindam.jpg'},
+    {'WallPaper':'lib/asstes/images/braden.jpg'},
+    {'WallPaper':'lib/asstes/images/eberhardgross.jpg'},
+    {'WallPaper':'lib/asstes/images/elaborate.jpg'},
+    {'WallPaper':'lib/asstes/images/ishan.jpg'},
+    {'WallPaper':'lib/asstes/images/jade.jpg'},
+    {'WallPaper':'lib/asstes/images/jeremy.jpg'},
+    {'WallPaper':'lib/asstes/images/laia-nunez.jpg'},
+    {'WallPaper':'lib/asstes/images/milky.jpg'},
+    {'WallPaper':'lib/asstes/images/mountain.jpg'},
+    {'WallPaper':'lib/asstes/images/oliver.jpg'},
+    {'WallPaper':'lib/asstes/images/peter (1).jpg'},
+    {'WallPaper':'lib/asstes/images/pexels.jpg'},
+    {'WallPaper':'lib/asstes/images/pexels (1).jpg'},
+    {'WallPaper':'lib/asstes/images/pexels-.jpg'},
+    {'WallPaper':'lib/asstes/images/pexels-alexazabache (1).jpg'},
+    {'WallPaper':'lib/asstes/images/pexels-aronvisuals.jpg'},
+    {'WallPaper':'lib/asstes/images/pexels-aslak-sonde.jpg'},
+    {'WallPaper':'lib/asstes/images/pexels-felixmit.jpg'},
+  ];
+}
+class Landscap {
+  static final List<Map<String,dynamic>>pic =[
+    {'WallPaper': 'lib/asstes/images/pexels-kc2.jpg'},
+    {'WallPaper':'lib/asstes/images/pexels-michael.jpg'},
+    {'WallPaper':'lib/asstes/images/pexels-p.jpg'},
+    {'WallPaper':'lib/asstes/images/pexels-stijn.jpg'},
+    {'WallPaper':'lib/asstes/images/pexels-wendelin.jpg'},
+    {'WallPaper':'lib/asstes/images/robert.jpg'},
+    {'WallPaper':'lib/asstes/images/san-fran (1).jpg'},
+    {'WallPaper':'lib/asstes/images/tree.jpg'},
+    {'WallPaper':'lib/asstes/images/download (2).jpg'},
+    {'WallPaper':'lib/asstes/images/download (1).jpg'},
+    {'WallPaper':'lib/asstes/images/images.jpg'},
+    {'WallPaper':'lib/asstes/images/images (1).jpg'},
+    {'WallPaper':'lib/asstes/images/images (1).jpg'},
+    {'WallPaper':'lib/asstes/images/images (1).jpg'},
+    {'WallPaper':'lib/asstes/images/mayank.jpg'},
+    {'WallPaper':'lib/asstes/images/m2.jpg'},
+    {'WallPaper':'lib/asstes/images/m3.jpg'},
+    {'WallPaper':'lib/asstes/images/m4.jpg'},
+    {'WallPaper':'lib/asstes/images/j2.jpg'},
+    {'WallPaper':'lib/asstes/images/j.jpg'},
+  ];
+}
+
+
+
+
+
+class ColorPage {
+  static final List<Map<String,dynamic>>kamal=[
+    {'WallPaper':Colors.brown},
+    {'WallPaper':Colors.blue},
+    {'WallPaper':Colors.cyanAccent},
+    {'WallPaper':Colors.indigo},
+    {'WallPaper':Colors.redAccent},
+    {'WallPaper':Colors.deepPurpleAccent},
+    {'WallPaper':Colors.deepPurple},
+    {'WallPaper':Colors.lightGreenAccent},
+    {'WallPaper':Colors.tealAccent},
+    {'WallPaper':Colors.teal},
+    {'WallPaper':Colors.yellow},
+    {'WallPaper':Colors.grey},
+    {'WallPaper':Colors.blueAccent},
+    {'WallPaper':Colors.pink},
+    {'WallPaper':Colors.orange},
+    {'WallPaper':Colors.lime},
+  ];
+}
